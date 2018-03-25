dca1e5abdba39116013e4a65a7794050b585aa79
Genteure <genteure@gmail.com>
2018-03-25 11:50:36 +0800

    again.

--- CI/push_clickonce.ps1
+++ CI/push_clickonce.ps1
@@ -3,6 +3,6 @@ if (-not (Test-Path env:APPVEYOR)) {
 }
 git clone --depth 1 https://github.com/Bililive/soft.danmuji.org.git C:\projects\site
 Get-ChildItem -Path .\BililiveRecorder.WPF\bin\Release\app.publish | Copy-Item -Destination C:\projects\site\BililiveRecorder -Recurse -Container
-git --quiet --git-dir=C:\projects\site\.git\ --work-tree=C:\projects\site\ add -A
-git --quiet --git-dir=C:\projects\site\.git\ --work-tree=C:\projects\site\ commit -m "BililiveRecorder $env:p_version"
-git --quiet --git-dir=C:\projects\site\.git\ --work-tree=C:\projects\site\ push
+git --git-dir=C:\projects\site\.git\ --work-tree=C:\projects\site\ add --quiet -A
+git --git-dir=C:\projects\site\.git\ --work-tree=C:\projects\site\ commit --quiet -m "BililiveRecorder $env:p_version"
+git --git-dir=C:\projects\site\.git\ --work-tree=C:\projects\site\ push --quiet 
