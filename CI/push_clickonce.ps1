38800b8cac08a2755e6e41b76f45c879bc2f63e5
Genteure <genteure@gmail.com>
2018-03-25 10:25:04 +0800

    fix test again

--- CI/push_clickonce.ps1
+++ CI/push_clickonce.ps1
@@ -1,5 +1,7 @@
-if(-not (Test-Path env:APPVEYOR)) Throw New-Object System.NotSupportedException "Not Running on Appveyor!"
-$env:old_script_path=(Get-Item -Path ".\" -Verbose).FullName
+if (-not (Test-Path env:APPVEYOR)) {
+    Throw New-Object System.NotSupportedException "Not Running on Appveyor!"
+}
+$env:old_script_path = (Get-Item -Path ".\" -Verbose).FullName
 git clone --depth 1 https://github.com/Bililive/soft.danmuji.org.git C:\projects\site
 Get-ChildItem -Path .\BililiveRecorder.WPF\bin\Release\app.publish | Copy-Item -Destination C:\projects\site\BililiveRecorder -Recurse -Container
 git --git-dir=C:\projects\site\.git\ --work-tree=C:\projects\site\ add -A
