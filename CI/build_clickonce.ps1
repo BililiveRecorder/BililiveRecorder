879cd97a1e44b3f7ac6719803c4d2809e4cc23dd
Genteure <genteure@gmail.com>
2018-03-25 11:24:34 +0800

    应该没问题了

--- CI/build_clickonce.ps1
+++ CI/build_clickonce.ps1
@@ -1,6 +1,6 @@
 if ($env:APPVEYOR) {
-    msbuild "BililiveRecorder.sln" /t:Publish /verbosity:minimal /p:Configuration=Release /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+    msbuild "BililiveRecorder.sln" /t:Publish /verbosity:minimal /p:Configuration=Release /p:ApplicationVersion="$env:p_version" /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 }else {
-    msbuild "BililiveRecorder.sln" /t:Publish /verbosity:minimal /p:Configuration=Release
+    msbuild "BililiveRecorder.sln" /t:Publish /verbosity:minimal /p:Configuration=Release /p:ApplicationVersion="$env:p_version"
 }
 if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
--- CI/push_clickonce.ps1
+++ CI/push_clickonce.ps1
@@ -1,9 +1,8 @@
 if (-not (Test-Path env:APPVEYOR)) {
     Throw New-Object System.NotSupportedException "Not Running on Appveyor!"
 }
-$env:old_script_path = (Get-Item -Path ".\" -Verbose).FullName
 git clone --depth 1 https://github.com/Bililive/soft.danmuji.org.git C:\projects\site
 Get-ChildItem -Path .\BililiveRecorder.WPF\bin\Release\app.publish | Copy-Item -Destination C:\projects\site\BililiveRecorder -Recurse -Container
-git --git-dir=C:\projects\site\.git\ --work-tree=C:\projects\site\ add -A
-git --git-dir=C:\projects\site\.git\ --work-tree=C:\projects\site\ commit -m 'BililiveRecorder $env:p_version'
-git --git-dir=C:\projects\site\.git\ --work-tree=C:\projects\site\ push
+git -q --git-dir=C:\projects\site\.git\ --work-tree=C:\projects\site\ add -A
+git -q --git-dir=C:\projects\site\.git\ --work-tree=C:\projects\site\ commit -m "BililiveRecorder $env:p_version"
+git -q --git-dir=C:\projects\site\.git\ --work-tree=C:\projects\site\ push
