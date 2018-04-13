e6d78ed3a71cb776f1a1652a90bb9c02f85ada60
Genteure <genteure@gmail.com>
2018-04-14 07:15:36 +0800

    fix

--- CI/appveyor_after_build.ps1
+++ CI/appveyor_after_build.ps1
@@ -1,4 +1,4 @@
-if ($env:BILILIVERECORDER_RELEASE)
+if ($env:BILILIVERECORDER_RELEASE.ToLower() -eq "true")
 {
     msbuild /t:Publish /verbosity:minimal /p:Configuration=Release /p:ApplicationVersion="$env:p_version" /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
     $host.SetShouldExit($LastExitCode)
