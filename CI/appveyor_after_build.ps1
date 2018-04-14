f2ba0ada0606cbda5873815d6cfb18e45b3e8c3e
Genteure <genteure@gmail.com>
2018-04-14 09:36:14 +0800

    code sign

--- CI/appveyor_after_build.ps1
+++ CI/appveyor_after_build.ps1
@@ -1,7 +1,7 @@
 if ($env:BILILIVERECORDER_RELEASE.ToLower() -eq "true")
 {
     Unprotect-File './CI/rixCloud2Genteure.pfx.AES' -KeyAsPlainText "$env:codesignaes"
-    Copy-Item .\CI\rixCloud2Genteure.pfx .\BililiveRecorder.WPF\
+    Import-PfxCertificate -FilePath C:\mypfx.pfx -CertStoreLocation Cert:\LocalMachine\My -Password "$env:codesignpasswd"
 
     msbuild /t:Publish /verbosity:minimal /p:Configuration=Release /p:ApplicationVersion="$env:p_version" /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
     $host.SetShouldExit($LastExitCode)
--- CI/test.ps1
+++
@@ -1,8 +0,0 @@
-if ($env:BILILIVERECORDER_RELEASE.ToLower() -eq "true")
-{
-    Write-Host "test"
-}
-else
-{
-    Write-Host "Not a new release, skipping msbuild /t:Publish"
-}
