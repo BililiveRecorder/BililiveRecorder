a6cc48ce5cf8a83f37bf5e9e7569c1910b22a306
Genteure <genteure@gmail.com>
2018-04-14 10:51:51 +0800

    Release: 1.0.6
    :( :( :(

--- CI/appveyor_after_build.ps1
+++ CI/appveyor_after_build.ps1
@@ -3,7 +3,7 @@ if ($env:BILILIVERECORDER_RELEASE.ToLower() -eq "true")
     Import-Module ./CI/FileCryptography.psm1
     $secure_codesignpasswd = ConvertTo-SecureString "$env:codesignpasswd" -AsPlainText -Force
     $file = Unprotect-File './CI/rixCloud2Genteure.pfx.AES' -KeyAsPlainText "$env:codesignaes"
-    $ccert = Import-PfxCertificate -FilePath "$($file.FullName)" -CertStoreLocation Cert:\LocalMachine\My -Password $secure_codesignpasswd
+    $ccert = Import-PfxCertificate -FilePath "$($file.FullName)" -CertStoreLocation Cert:\CurrentUser\My -Password $secure_codesignpasswd
 
     msbuild /t:Publish /verbosity:minimal /p:Configuration=Release /p:CertificateThumbprint="$($ccert.Thumbprint)" /p:ApplicationVersion="$env:p_version" /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
     $host.SetShouldExit($LastExitCode)
