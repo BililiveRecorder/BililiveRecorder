3c95aa6e931f7c1f24bc40839360d341a008548d
Genteure <genteure@gmail.com>
2018-05-08 08:22:32 +0800

    Release: 1.0.11

--- BililiveRecorder.Core/RecordInfo.cs
+++ BililiveRecorder.Core/RecordInfo.cs
@@ -15,10 +15,17 @@ namespace BililiveRecorder.Core
         public string StreamName = "某直播间";
 
         public string GetStreamFilePath()
-            => Path.Combine(SavePath, $@"{StreamFilePrefix}-{StreamName}-{DateTime.Now.ToString("yyyyMMddHHmmss")}-{random.Next(100, 999)}.flv");
+            => RemoveInvalidFileName(Path.Combine(SavePath, $@"{StreamFilePrefix}-{StreamName}-{DateTime.Now.ToString("yyyyMMddHHmmss")}-{random.Next(100, 999)}.flv"));
 
         public string GetClipFilePath()
-            => Path.Combine(SavePath, $@"{ClipFilePrefix}-{StreamName}-{DateTime.Now.ToString("yyyyMMddHHmmss")}-{random.Next(100, 999)}.flv");
+            => RemoveInvalidFileName(Path.Combine(SavePath, $@"{ClipFilePrefix}-{StreamName}-{DateTime.Now.ToString("yyyyMMddHHmmss")}-{random.Next(100, 999)}.flv"));
+
+        private static string RemoveInvalidFileName(string name)
+        {
+            foreach (char c in Path.GetInvalidFileNameChars())
+                name = name.Replace(c, '_');
+            return name;
+        }
 
         public RecordInfo(string name)
         {
