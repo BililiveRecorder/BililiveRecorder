c89de6a130666eea28a127bc3d85ec57feb2315c
Genteure <genteure@gmail.com>
2018-11-07 07:14:00 +0800

    添加自动切割设置界面

--- BililiveRecorder.WPF/ValueConverters.cs
+++ BililiveRecorder.WPF/ValueConverters.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Windows.Data;
 
 namespace BililiveRecorder.WPF
 {
-    class EnumToBooleanConverter : IValueConverter
+    internal class ValueConverterGroup : List<IValueConverter>, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return this.Aggregate(value, (current, converter) => converter.Convert(current, targetType, parameter, culture));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class EnumToBooleanConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
@@ -17,7 +32,20 @@ namespace BililiveRecorder.WPF
         }
     }
 
-    class RecordStatusConverter : IMultiValueConverter
+    internal class BooleanInverterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+    }
+
+    internal class RecordStatusConverter : IMultiValueConverter
     {
         public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
         {
