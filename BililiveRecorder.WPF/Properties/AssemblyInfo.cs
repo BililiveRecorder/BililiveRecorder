d603e0f7b2586c1159fab71121476aff01d36394
Genteure <genteure@gmail.com>
2018-11-07 13:47:24 +0800

    一些小项目修改

--- BililiveRecorder.WPF/Properties/AssemblyInfo.cs
+++ BililiveRecorder.WPF/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Genteure")]
 [assembly: AssemblyProduct("B站录播姬")]
-[assembly: AssemblyCopyright("Copyright Genteure ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018 Genteure")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
