3a996373563d00533e54f6db28a62158162a59cb
Genteure <genteure@gmail.com>
2019-01-31 14:17:38 +0800

    增加 workdirectory 命令行选项

---
+++ BililiveRecorder.WPF/CommandLineOption.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+namespace BililiveRecorder.WPF
+{
+    public class CommandLineOption
+    {
+        [Option('w', "workdirectory", Default = null, HelpText = "设置工作目录并跳过选择目录 GUI", Required = false)]
+        public string WorkDirectory { get; set; }
+    }
+}
