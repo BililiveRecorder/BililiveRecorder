5473991c8b091a0927ea6e2f9983e165a97783fe
Genteure <genteure@gmail.com>
2019-04-15 23:27:48 +0800

    增加退出确认

---
+++ BililiveRecorder.WPF/TimedMessageBox.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace BililiveRecorder.WPF
+{
+    /// <summary>
+    /// TimedMessageBox.xaml 的交互逻辑
+    /// </summary>
+    public partial class TimedMessageBox : Window, INotifyPropertyChanged
+    {
+        public string Message { get => _message; set => SetField(ref _message, value); }
+        private string _message = string.Empty;
+
+        public int CountDown { get => _countdown; set => SetField(ref _countdown, value); }
+        private int _countdown = 10;
+
+        private DispatcherTimer timer;
+
+        public TimedMessageBox()
+        {
+            timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1), IsEnabled = false };
+            timer.Tick += (sender, e) =>
+            {
+                CountDown -= 1;
+                if (CountDown <= 0)
+                {
+                    Cancel();
+                    timer.Stop();
+                    timer = null;
+                }
+            };
+
+            DataContext = this;
+
+            InitializeComponent();
+        }
+
+        private void ConfirmClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void CancelClick(object sender, RoutedEventArgs e) => Cancel();
+
+        private void Cancel()
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer?.Start();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) { return false; }
+            field = value; OnPropertyChanged(propertyName); return true;
+        }
+    }
+}
