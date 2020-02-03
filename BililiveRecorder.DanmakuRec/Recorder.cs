801d19865788b5d1ab1e6fa0f6ae2b7d987eb2e0
dengbw01 <dengbw01@outlook.com>
2020-02-04 00:15:15 +0800

    开始添加弹幕录制功能
    很晚了先睡了、、、

---
+++ BililiveRecorder.DanmakuRec/Recorder.cs
@@ -0,0 +1,30 @@
+﻿using BililiveRecorder.Core;
+using System;
+using System.Collections.Generic;
+
+namespace BililiveRecorder.DanmakuRec
+{
+    public class Recorder
+    {
+        public event ReceivedDanmakuEvt ReceivedDanmaku;
+        public List<MsgTypeEnum> record_filter;
+
+        /// <summary>
+        /// 装载弹幕记录系统
+        /// </summary>
+        /// <param name="filter">记录哪些类型的弹幕</param>
+        public Recorder(List<MsgTypeEnum> filter)
+        {
+            record_filter = filter;
+            ReceivedDanmaku += Receiver_ReceivedDanmaku;
+        }
+
+        private void Receiver_ReceivedDanmaku(object sender, ReceivedDanmakuArgs e)
+        {
+            if (record_filter.Contains(e.Danmaku.MsgType))//符合要记录的类型
+            {
+
+            }
+        }
+    }
+}
