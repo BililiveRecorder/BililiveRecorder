f489a8d2296c129bc9a88ec6f516e46a59408b19
dengbw01 <dengbw01@outlook.com>
2020-02-04 09:39:35 +0800

    修正了一些代码问题
    修正了错误的代码结构和事件触发

--- BililiveRecorder.DanmakuRec/Recorder.cs
+++ BililiveRecorder.DanmakuRec/Recorder.cs
@@ -6,17 +6,16 @@ namespace BililiveRecorder.DanmakuRec
 {
     public class Recorder
     {
-        public event ReceivedDanmakuEvt ReceivedDanmaku;
         public List<MsgTypeEnum> record_filter;
 
         /// <summary>
         /// 装载弹幕记录系统
         /// </summary>
         /// <param name="filter">记录哪些类型的弹幕</param>
-        public Recorder(List<MsgTypeEnum> filter)
+        public Recorder(StreamMonitor monitor,List<MsgTypeEnum> filter)
         {
             record_filter = filter;
-            ReceivedDanmaku += Receiver_ReceivedDanmaku;
+            monitor.ReceivedDanmaku += Receiver_ReceivedDanmaku;
         }
 
         private void Receiver_ReceivedDanmaku(object sender, ReceivedDanmakuArgs e)
