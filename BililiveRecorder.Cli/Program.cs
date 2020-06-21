e25c27c8b72443f65fb89482c70927c88e5170f3
Misty <gyc990326@gmail.com>
2020-06-21 21:26:51 +0800

    Support more commandline options

--- BililiveRecorder.Cli/Program.cs
+++ BililiveRecorder.Cli/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Autofac;
 using BililiveRecorder.Core;
+using BililiveRecorder.Core.Config;
 using BililiveRecorder.FlvProcessor;
 using CommandLine;
 using NLog;
@@ -22,7 +24,9 @@ namespace BililiveRecorder.Cli
             var builder = new ContainerBuilder();
             builder.RegisterModule<FlvProcessorModule>();
             builder.RegisterModule<CoreModule>();
+            builder.RegisterType<CommandConfigV1>().As<ConfigV1>().InstancePerMatchingLifetimeScope("recorder_root");
             Container = builder.Build();
+
             RootScope = Container.BeginLifetimeScope("recorder_root");
             Recorder = RootScope.Resolve<IRecorder>();
             if (!Recorder.Initialize(System.IO.Directory.GetCurrentDirectory()))
@@ -30,24 +34,21 @@ namespace BililiveRecorder.Cli
                 Console.WriteLine("Initialize Error");
                 return;
             }
+
             Parser.Default
-                .ParseArguments<CommandLineOption>(Environment.GetCommandLineArgs())
+                .ParseArguments<CommandConfigV1>(() => (CommandConfigV1)Recorder.Config, Environment.GetCommandLineArgs())
                 .WithParsed(Run);
         }
 
-        private static void Run(CommandLineOption option)
+        private static void Run(ConfigV1 option)
         {
-            Recorder.Config.AvoidTxy = true;
-
-            if (option.RoomID != null)
+            foreach (var room in option.RoomList)
             {
-                int roomid = (int)option.RoomID;
-                if (Recorder.Where(r => r.RoomId == roomid).Count() == 0)
+                if (Recorder.Where(r => r.RoomId == room.Roomid).Count() == 0)
                 {
-                    Recorder.AddRoom(roomid);
+                    Recorder.AddRoom(room.Roomid);
                 }
             }
-            
             
             logger.Info("开始录播");
             Task.WhenAll(Recorder.Select(x => Task.Run(() => x.Start()))).Wait();
@@ -63,9 +64,49 @@ namespace BililiveRecorder.Cli
         }
     }
 
-    public class CommandLineOption
+    class ConfigV1Metadata
     {
-        [Option('i', "id", HelpText = "room id", Required = false)]
-        public int? RoomID { get; set; }
+        [Option('o', "dir", Default = ".", HelpText = "Output directory", Required = false)]
+        [Utils.DoNotCopyProperty]
+        public object WorkDirectory { get; set; }
+
+        [Option("cookie", HelpText = "Provide custom cookies", Required = false)]
+        public object Cookie { get; set; }
+
+        [Option("avoidtxy", HelpText = "Avoid Tencent Cloud server", Required = false)]
+        public object AvoidTxy { get; set; }
+
+        [Option("live_api_host", HelpText = "Use custom api host", Required = false)]
+        public object LiveApiHost { get; set; }
+
+        [Option("record_filename_format", HelpText = "Recording name format", Required = false)]
+        public object RecordFilenameFormat { get; set; }
+
+
+
+    }
+
+    [MetadataType(typeof(ConfigV1Metadata))]
+    class CommandConfigV1 : ConfigV1
+    {
+        [Option('i', "id", HelpText = "room id", Required = true)]
+        [Utils.DoNotCopyProperty]
+        public string _RoomList
+        {
+            set
+            {
+                var roomids = value.Split(',');
+                RoomList.Clear();
+
+                foreach (var roomid in roomids)
+                {
+                    var room = new RoomV1();
+                    room.Roomid = Int32.Parse(roomid);
+                    room.Enabled = false;
+                    RoomList.Add(room);
+                }
+            }
+
+        }
     }
 }
