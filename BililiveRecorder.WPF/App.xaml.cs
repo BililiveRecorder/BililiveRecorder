37086776b65036fe87994632606103cb2d31f175
Genteure <genteure@gmail.com>
2021-03-18 23:05:48 +0800

    WPF: Log SystemEvents

--- BililiveRecorder.WPF/App.xaml.cs
+++ BililiveRecorder.WPF/App.xaml.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Windows;
+using Microsoft.Win32;
+using Serilog;
 
 #nullable enable
 namespace BililiveRecorder.WPF
@@ -8,11 +11,47 @@ namespace BililiveRecorder.WPF
     /// </summary>
     public partial class App : Application
     {
+        private readonly ILogger loggerSystemEvents = Log.ForContext<SystemEvents>();
+
         private void Application_SessionEnding(object sender, SessionEndingCancelEventArgs e)
         {
             if (e != null)
                 e.Cancel = true;
             (Current.MainWindow as NewMainWindow)?.CloseWithoutConfirmAction();
         }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                SystemEvents.TimeChanged += this.SystemEvents_TimeChanged;
+                SystemEvents.SessionEnding += this.SystemEvents_SessionEnding;
+                SystemEvents.PowerModeChanged += this.SystemEvents_PowerModeChanged;
+            }
+            catch (Exception ex)
+            {
+                this.loggerSystemEvents.Error(ex, "Error attaching event");
+            }
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            try
+            {
+                SystemEvents.TimeChanged -= this.SystemEvents_TimeChanged;
+                SystemEvents.SessionEnding -= this.SystemEvents_SessionEnding;
+                SystemEvents.PowerModeChanged -= this.SystemEvents_PowerModeChanged;
+            }
+            catch (Exception ex)
+            {
+                this.loggerSystemEvents.Error(ex, "Error detaching event");
+            }
+        }
+
+        private void SystemEvents_SessionEnding(object sender, SessionEndingEventArgs e) => this.loggerSystemEvents.Debug("SessionEnding {Reason}", e.Reason);
+
+        private void SystemEvents_TimeChanged(object sender, System.EventArgs e) => this.loggerSystemEvents.Debug("TimeChanged");
+
+        private void SystemEvents_PowerModeChanged(object sender, PowerModeChangedEventArgs e) => this.loggerSystemEvents.Warning("PowerModeChanged {Mode}", e.Mode);
     }
 }
