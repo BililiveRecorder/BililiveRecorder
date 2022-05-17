BililiveRecorder/BililiveRecorder

commit 730bc9a4d12625ebacebfa2cca0aa1e2d269e3f8

Core: Change visibility of most types to internal

BililiveRecorder.Core/PollyPolicy.cs
@@ -15,7 +15,7 @@ namespace BililiveRecorder.Core
     {
         private static readonly ILogger logger = Log.ForContext<PollyPolicy>();
 
-        public PollyPolicy()
+        internal PollyPolicy()
         {
             this.IpBlockedHttp412CircuitBreakerPolicy = Policy
                 .Handle<Http412Exception>()

BililiveRecorder.Core/Room.cs
@@ -19,7 +19,7 @@ using Timer = System.Timers.Timer;
 
 namespace BililiveRecorder.Core
 {
-    public class Room : IRoom
+    internal class Room : IRoom
     {
         private readonly object recordStartLock = new object();
         private readonly SemaphoreSlim recordRetryDelaySemaphoreSlim = new SemaphoreSlim(1, 1);
@@ -51,7 +51,7 @@ namespace BililiveRecorder.Core
         private DateTimeOffset danmakuClientConnectTime;
         private static readonly TimeSpan danmakuClientReconnectNoDelay = TimeSpan.FromMinutes(1);
 
-        public Room(IServiceScope scope, RoomConfig roomConfig, int initDelayFactor, ILogger logger, IDanmakuClient danmakuClient, IApiClient apiClient, IBasicDanmakuWriter basicDanmakuWriter, IRecordTaskFactory recordTaskFactory)
+        internal Room(IServiceScope scope, RoomConfig roomConfig, int initDelayFactor, ILogger logger, IDanmakuClient danmakuClient, IApiClient apiClient, IBasicDanmakuWriter basicDanmakuWriter, IRecordTaskFactory recordTaskFactory)
         {
             this.scope = scope ?? throw new ArgumentNullException(nameof(scope));
             this.RoomConfig = roomConfig ?? throw new ArgumentNullException(nameof(roomConfig));

BililiveRecorder.WPF/Program.cs
@@ -280,7 +280,7 @@ namespace BililiveRecorder.WPF
 
         private class SentryEventProcessor : ISentryEventProcessor
         {
-            private static readonly string JintConsole = typeof(Core.Scripting.Runtime.JintConsole).FullName;
+            private const string JintConsole = "Core.Scripting.Runtime.JintConsole";
             private static readonly string UserScriptRunner = typeof(Core.Scripting.UserScriptRunner).FullName;
             public SentryEvent? Process(SentryEvent e) => (e?.Logger == JintConsole || e?.Logger == UserScriptRunner) ? null : e;
         }

test/BililiveRecorder.Core.UnitTests/VerifyConfig.cs
@@ -1,0 +1,18 @@
+using System.IO;
+using System.Runtime.CompilerServices;
+using DiffEngine;
+using VerifyTests;
+
+namespace BililiveRecorder.Core.UnitTests
+{
+    public static class VerifyConfig
+    {
+        [ModuleInitializer]
+        public static void Init()
+        {
+            VerifierSettings.DerivePathInfo(Expectations.Initialize);
+            VerifierSettings.ModifySerialization(_ => _.IgnoreMembersWithType<Stream>());
+            DiffRunner.Disabled = true;
+        }
+    }
+}

commit a993fca68389c4520914c3de924ec7ce226cbaab

Fix bug introduced in 730bc9a
730bc9a4d12625ebacebfa2cca0aa1e2d269e3f8

BililiveRecorder.Core/PollyPolicy.cs
@@ -15,7 +15,7 @@ namespace BililiveRecorder.Core
     {
         private static readonly ILogger logger = Log.ForContext<PollyPolicy>();
 
-        internal PollyPolicy()
+        public PollyPolicy()
         {
             this.IpBlockedHttp412CircuitBreakerPolicy = Policy
                 .Handle<Http412Exception>()
@@ -68,7 +68,6 @@ namespace BililiveRecorder.Core
             this[PolicyNames.PolicyRoomInfoApiRequestAsync] = Policy.WrapAsync(bulkhead, retry, this.IpBlockedHttp412CircuitBreakerPolicy, this.RequestFailedCircuitBreakerPolicy);
             this[PolicyNames.PolicyDanmakuApiRequestAsync] = Policy.WrapAsync(cachePolicy, bulkhead, retry, this.IpBlockedHttp412CircuitBreakerPolicy, this.RequestFailedCircuitBreakerPolicy);
             this[PolicyNames.PolicyStreamApiRequestAsync] = Policy.WrapAsync(bulkhead, retry, this.IpBlockedHttp412CircuitBreakerPolicy, this.RequestFailedCircuitBreakerPolicy);
-
         }
 
         public readonly MemoryCache memoryCache;

BililiveRecorder.Core/Room.cs
@@ -51,7 +51,7 @@ namespace BililiveRecorder.Core
         private DateTimeOffset danmakuClientConnectTime;
         private static readonly TimeSpan danmakuClientReconnectNoDelay = TimeSpan.FromMinutes(1);
 
-        internal Room(IServiceScope scope, RoomConfig roomConfig, int initDelayFactor, ILogger logger, IDanmakuClient danmakuClient, IApiClient apiClient, IBasicDanmakuWriter basicDanmakuWriter, IRecordTaskFactory recordTaskFactory)
+        public Room(IServiceScope scope, RoomConfig roomConfig, int initDelayFactor, ILogger logger, IDanmakuClient danmakuClient, IApiClient apiClient, IBasicDanmakuWriter basicDanmakuWriter, IRecordTaskFactory recordTaskFactory)
         {
             this.scope = scope ?? throw new ArgumentNullException(nameof(scope));
             this.RoomConfig = roomConfig ?? throw new ArgumentNullException(nameof(roomConfig));

BililiveRecorder.WPF/Program.cs
@@ -280,7 +280,7 @@ namespace BililiveRecorder.WPF
 
         private class SentryEventProcessor : ISentryEventProcessor
         {
-            private const string JintConsole = "Core.Scripting.Runtime.JintConsole";
+            private const string JintConsole = "BililiveRecorder.Core.Scripting.Runtime.JintConsole";
             private static readonly string UserScriptRunner = typeof(Core.Scripting.UserScriptRunner).FullName;
             public SentryEvent? Process(SentryEvent e) => (e?.Logger == JintConsole || e?.Logger == UserScriptRunner) ? null : e;
         }

test/BililiveRecorder.Core.UnitTests/VerifyConfig.cs
@@ -12,7 +12,8 @@ namespace BililiveRecorder.Core.UnitTests
         {
             VerifierSettings.DerivePathInfo(Expectations.Initialize);
             VerifierSettings.ModifySerialization(_ => _.IgnoreMembersWithType<Stream>());
-            DiffRunner.Disabled = true;
+            DiffRunner.Disabled = false;
+            DiffTools.UseOrder(DiffTool.VisualStudioCode, DiffTool.Rider, DiffTool.WinMerge, DiffTool.VisualStudio);
         }
     }
 }

test/BililiveRecorder.Flv.Tests/VerifyConfig.cs
@@ -12,7 +12,8 @@ namespace BililiveRecorder.Flv.Tests
         {
             VerifierSettings.DerivePathInfo(Expectations.Initialize);
             VerifierSettings.ModifySerialization(_ => _.IgnoreMembersWithType<Stream>());
-            DiffRunner.Disabled = true;
+            DiffRunner.Disabled = false;
+            DiffTools.UseOrder(DiffTool.VisualStudioCode, DiffTool.Rider, DiffTool.WinMerge, DiffTool.VisualStudio);
         }
     }
 }