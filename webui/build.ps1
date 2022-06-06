03ca7be6447a0ebf84080ffdacdaa34fd502b0f1
genteure <genteure@gmail.com>
2022-06-07 00:55:50 +0800

    Update webui and enable EMBEDED_BUILD flag

--- webui/build.ps1
+++ webui/build.ps1
@@ -1,6 +1,7 @@
 Set-PSDebug -Trace 1
 Push-Location "$PSScriptRoot/source"
 $env:BASE_URL = "/ui/"
+$env:EMBEDED_BUILD = "true"
 npm ci
 npx vite build
 Copy-Item -Recurse dist ../../BililiveRecorder.Web/embeded/ui
--- webui/build.sh
+++ webui/build.sh
@@ -3,6 +3,7 @@ set -ex
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 pushd "$SCRIPT_DIR/source"
 export BASE_URL="/ui/"
+export EMBEDED_BUILD="true"
 npm ci && npx vite build
 cp --recursive dist ../../BililiveRecorder.Web/embeded/ui
 popd
