4472ad5d53efcb00e13618fb0d898ea32e9558e6
shugen002 <shugen002@users.noreply.github.com>
2022-08-29 01:51:41 +0800

    fix: wrong BASE_URL (+1 squashed commits)
    Squashed commits:
    
    [485c2bd] fix: wrong BASE_URL

--- webui/build.ps1
+++ webui/build.ps1
@@ -1,6 +1,6 @@
 Set-PSDebug -Trace 1
 Push-Location "$PSScriptRoot/source"
-$env:BASE_URL = "/ui/"
+$env:BASE_URL = "./"
 $env:VITE_EMBEDDED_BUILD = "true"
 (npm ci) -and (npx vite build)
 Remove-Item -Recurse -ErrorAction SilentlyContinue ../../BililiveRecorder.Web/embeded/ui
--- webui/build.sh
+++ webui/build.sh
@@ -2,7 +2,7 @@
 set -ex
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 pushd "$SCRIPT_DIR/source"
-export BASE_URL="/ui/"
+export BASE_URL="./"
 export VITE_EMBEDDED_BUILD="true"
 npm ci && npx vite build
 rm -R ../../BililiveRecorder.Web/embeded/ui 2>/dev/null || true
